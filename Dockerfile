e147281f610b4b732fbb11dbdc0fb5d91d3f35ab
sergi.ortiz <sergi.ortiz.rodriguez@gmail.com>
2018-04-22 11:23:46 +0200

    Spider of results working. Dockerfile in test
    Already extracting match results properly and storing in a .csv file.
    Now working in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,10 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
+# esto es un try
+MV ['./files/quiniela_as_results.csv', './old_resulsts/quiniela_as_results.csv']
+
 WORKDIR ./quiniela_crawler
 
-CMD ["scrapy crawl quiniela"]
+# lanzamos la spyder.
+CMD ["scrapy crawl quiniela -o ../files/quiniela_as_results.csv -t csv"]
--- quiniela_crawler/quiniela_crawler/spiders/quiniela_spider.py
+++ quiniela_crawler/quiniela_crawler/spiders/quiniela_spider.py
@@ -1,25 +1,18 @@
-import os
-import logging.config
-from logging.config import fileConfig
 import scrapy
 
-if not os.path.isdir('logs'):
-    os.makedirs('logs')
-if not os.path.isfile('logs/python.log'):
-    os.mknod('logs/python.log')
-
-logger = logging.getLogger(__name__)
-fileConfig('logger.ini')
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+"""
+Crawl results URLs such as
+https://resultados.as.com/resultados/futbol/primera/2017_2018/jornada/regular_a_30/
+and extract match and result.
+"""
 
 
 class BlogSpider(scrapy.Spider):
     name = 'quiniela'
-    jornadas = 38
 
     def start_requests(self):
         urls = []
+        jornadas = 38
 
         for i in range(1, jornadas+1):
             urls += ['https://resultados.as.com/resultados/futbol/primera/'
@@ -33,6 +26,8 @@ class BlogSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        for next_match in response.css('div.cont-resultado finalizado'):
-            yield {'match': next_match.css('a.title').extract_first(),
-                   'results': next_match.css('a ::text').extract_first()}
+        for match in response.xpath("//div[@class='cont-resultado finalizado']"):
+
+            yield {'jornada': response.url,
+                   'match': match.xpath("./a/@title").extract_first().strip(),
+                   'result': match.xpath("./a/text()").extract_first().strip()}
