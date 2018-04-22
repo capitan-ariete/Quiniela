8f7ccfb0caa86246c45a1fa17e77489cff5695e3
sergi.ortiz <sergi.ortiz.rodriguez@gmail.com>
2018-04-22 18:14:07 +0200

    Add predictor_accuracy seed file
    This script will give the accuracy of the predictor recursively

---
+++ predictor_accuracy.py
@@ -0,0 +1,24 @@
+import os
+import logging.config
+from logging.config import fileConfig
+
+if not os.path.isdir('logs'):
+    os.makedirs('logs')
+if not os.path.isfile('logs/python.log'):
+    os.mknod('logs/python.log')
+
+logger = logging.getLogger(__name__)
+fileConfig('logger.ini')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+def main():
+    logger.info('TODO')
+    return
+
+
+if __name__ == '__main__':
+    logger.info("It's show time.")
+    main()
+    logger.info("You have been terminated.")
