e38169ede1f3c66a9e7d0bab371a96c8b61398b1
sergi.ortiz <sergi.ortiz.rodriguez@gmail.com>
2018-09-02 19:43:14 +0200

    Refactor spider to crawl seassons since 2013-2014

--- quiniela_crawler/quiniela_crawler/spiders/quiniela_spider.py
+++ quiniela_crawler/quiniela_crawler/spiders/quiniela_spider.py
@@ -15,14 +15,32 @@ scrapy crawl quiniela -o ../files/files_new.csv
 class BlogSpider(scrapy.Spider):
     name = 'quiniela'
 
-    def start_requests(self):
-        jornadas = 38
+    @staticmethod
+    def _urls_generator():
+        """
+        Generate list of URLS to craw
+        :return:
+        """
+        list_years = list(zip(range(2013, 2019), range(2014, 2020)))
+        jornadas = range(1, 39)
         ligas = ['primera', 'segunda']
 
-        urls = ['https://resultados.as.com/resultados/futbol/' \
-                '{liga}/2017_2018/jornada/regular_a_{jornada}/'.format(jornada=j, liga=l)
-                for l in ligas
-                for j in range(1, jornadas + 1)]
+        urls = [f'https://resultados.as.com/resultados/futbol/' \
+                f'{liga}/' \
+                f'{start_year}_{end_year}/' \
+                f'jornada/regular_a_{jornada}/'
+                for liga in ligas
+                for jornada in jornadas
+                for start_year, end_year in list_years]
+
+        return urls
+
+    def start_requests(self):
+        """
+        Crawl leagues seassons from 2013-2014 onwards.
+        :return:
+        """
+        urls = _urls_generator()
 
         for url in urls:
             try:
@@ -31,7 +49,11 @@ class BlogSpider(scrapy.Spider):
                 logger.warning('URL {} does not exist'.format(url))
 
     def parse(self, response):
-
+        """
+        Parse the match result
+        :param response:
+        :return:
+        """
         for match in response.xpath("//div[@class='cont-resultado finalizado']"):
 
             d = {'jornada': response.url,
